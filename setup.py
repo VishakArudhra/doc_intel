9c14aa349f2679aabbba68ffee8bb5e9337dd7b8
vishak arudhra <vishakarudhra@gmail.com>
2021-09-16 14:11:54 +0530

    updated MANIFEST and setup py file

--- setup.py
+++ setup.py
@@ -10,17 +10,17 @@ README = (HERE / "README.md").read_text()
 # This call to setup() does all the work
 setup(
     name="doc_intel",
-    version="0.0.10",
+    version="0.0.11",
     description="Your solution to cleansing PDF documents for preprocessing for NLP",
     long_description=README,
     long_description_content_type="text/markdown",
-    url="https://github.com/VishakArudhra/detext",
+    url="https://github.com/VishakArudhra/doc_intel",
     author="Vishak Arudhra",
     author_email="vishakarudhra@gmail.com",
     license="MIT",
     classifiers=[
         "License :: OSI Approved :: MIT License",
-        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.x",
         "Programming Language :: Python :: 3.8",
     ],
     packages=["doc_intel"],
